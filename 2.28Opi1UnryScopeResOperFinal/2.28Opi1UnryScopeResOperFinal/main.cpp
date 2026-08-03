3a11b8d55feca1bf4f7308acfdd3eb1f741a3bc7
Oppie1 <atindall729@gmail.com>
2026-08-03 17:31:56 -0500

    Add files via upload

---
+++ 2.28Opi1UnryScopeResOperFinal/2.28Opi1UnryScopeResOperFinal/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+
+//Declare new global int variable named tuna then initialize it to 33.
+//CODE:
+
+//Declare and define void function tunaSentence with no parameters that outputs
+//"This is a global variable tuna, it uses a function outside main.
+//CODE:
+
+int main()
+{
+	//Declare a local int variable tuna (can be same name as global variable) initializes to 20.
+	//CODE:
+
+	//Output statement describing being within main() but accessing global tuna variable using 
+	//"::" -> scope resolution operator.
+	//CODE:
+
+	//Output statement using local variable within main() to output local tuna variable out to screen.
+	//CODE:
+
+	cout << "\n---I am example of function outside main using the global variable int tuna---\n\n" << endl;
+
+	//Call the function outside main.
+	//CODE:
+
+}
---
+++ 2.29OpiFuncOvrlodingFinal/2.29OpiFuncOvrlodingFinal/main.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+
+
+//Declare/define void function using printNumber() with int var as parameter. Output statement 
+//we are printing int to the screen using var.
+//CODE:
+
+//Declare/define same void function with a double var as its parameter. Output statement
+//saying printing double to screen with var.
+//CODE:
+
+//Declare/define same void function printNumber() with float var as its parameter. Output statement
+//saying printing float out to screen with var.
+//CODE:
+
+
+int main()
+{
+	//Declare 3 vars a, b and c representing int, float, double. Initialized to 54, 32.4896 and 3.141592653589 respectively.
+	//CODE:
+
+	//Call the function with "a" variable as its argument.
+	//CODE:
+
+	//Call function with "b" variable as its argument.
+	//CODE:
+
+	//Call function with "c" as its argument.
+	//CODE:
+
+	//Call function with "13" as its argument.
+	//CODE:
+
+	//Call function with 3.14 as its argument.
+	//CODE:
+
+}
