16f090ece5f091829f159f5309f155bf1025e7e9
Oppie1 <atindall729@gmail.com>
2026-09-21 14:13:18 -0500

    Add files via upload

---
+++ 2.60OpiTmpltSplzatnFinal/2.60OpiTmpltSplzatnFinal/main.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+//Declare a template for class T
+//CODE:
+
+//Declare an Adam class
+//CODE:
+
+
+//Declare public access specifier.
+//CODE:
+
+//Declare Adam constructor with template type T, variable x parameter outputs x " is not a character".
+//CODE:
+
+//Declare empty template <> declaring full template specialization
+//CODE:
+
+//Declare class Adam with "char" template parameter.
+//CODE:
+
+//Declare public access specifier.
+//CODE:
+
+//Declare Adam constructor with char data type and var x as parameter that outputs x "is a character".
+//CODE:
+
+
+int main() {
+
+	//Instantiate object ato of Adam class int template parameter, pass 7 as argument
+	//CODE:
+
+
+	//Instantiate object ato2 of Adam class with double template parameter, passing 3.1415 as argument.
+	//CODE:
+	
+
+	//Instantiate object ato3 of Adam class with char template parameter, passing in 'q' as argument.
+	//CODE:
+	
+
+}
