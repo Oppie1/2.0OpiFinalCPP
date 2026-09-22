f2618a10a5d916373c6c6b0ea1349bc395fa0cf9
Oppie1 <atindall729@gmail.com>
2026-09-21 22:25:03 -0500

    Add files via upload

---
+++ 2.61OpiExcptinsFinal/2.61OpiExcptinsFinal/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() {
+
+	cout << "Create error handler/code try/catch that will stop the program without crashing" << endl;
+
+	//Use try keyword to create codeblock that has vars and nested if statement with throw.
+	//CODE:
+
+	//Declare/initialize two storage vars that store ages momsAge and sonsAge.
+	//CODE:
+
+	//Use if statement to check sonsAge doesn't exceed mom's age. Within if statement use 
+	//throw keyword with number 99 to catch block or if not true skip if block.
+	//CODE:
+
+	//Use catch keyword with parameter of int x that will take int from throw and show
+	//output to screen saying Son cant be older than mom and ERROR NUMBER x
+	//CODE:
+
+	//Use try keyword again.
+	//CODE:
+
+	//Declare two int vars dadsAge and daughtersAge
+	//CODE:
+
+	//Output prompt to user for dads age an cin statement to retrieve/store in dadsAge var
+	//CODE:
+
+	//Output prompt user for daughters age and cin statement to retrieve/store data in daughtersAge Var
+	//CODE:
+
+	//Use if to check if daughtersAge is greater than dads. Then a throw keyword with 92
+	//that will execute (ERROR) if true.
+	//CODE:
+
+	//Output saying what dads age is with dadsAge var and same for daughter
+	//within try but after throw if statement.
+	//CODE:
+
+	//Use catch keyword with an int parameter var x
+	//CODE:
+
+	//Output saying dad can not be older than daughter with "x" error number
+	//CODE:
+
+}
