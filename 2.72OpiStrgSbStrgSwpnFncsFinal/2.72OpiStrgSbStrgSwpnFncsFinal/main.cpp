6326affcaeb4ac0d7f9f8840c5a196e853f236d5
Oppie1 <atindall729@gmail.com>
2026-09-24 17:41:06 -0500

    Add files via upload

---
+++ 2.72OpiStrgSbStrgSwpnFncsFinal/2.72OpiStrgSbStrgSwpnFncsFinal/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+int main() {
+
+	//Instantiate s1 obj of string class "I am initialization for s1 var/obj"
+	//CODE:
+
+	//Output call find() with "am" as argument on s1 object. From left (number).
+	//CODE:
+
+	// "r" find -> rfind() function it starts from the right looking for
+	//first instance of "am" from that direction. The index value of "am" from the right is 21.
+	//So finds it from right but the index number is still counted from left.
+	cout << "\nUsing rfind() function:" << endl;
+
+	//Output call rfind() on s1 object with "am" as the parameter.
+	//Second am from right number.
+	//CODE:
+
+	cout << "\nThis is an example of swapping (swap() function) one sentence with another." << endl;
+
+	//Instantiate obj sentenceOne of string class. Output: "This is a var/obj
+	//sentence/string that will be swapped with string/sentence two"
+	//CODE:
+
+	//Instantiate obj named sentenceTwo of string class with arg "This is var/obj that will 
+	//be swapped with sentenceOne
+	//CODE:
+
+	//Add a cout statement that displays sentence one and two (objects) on different lines.
+	//CODE:
+
+	//This is the swap() (located in string library) that we can use to perform swap.
+	//Call the swap() function on the sentenceOne object with object two as the argument.
+	//CODE:
+
+	//Output statement to print sentences one and two (also objects) to the screen.
+	//CODE:
+
+	cout << "\nThis is an example of taking sentence and pulling part of it out (sub string)\n" << endl;
+
+	//Instantiate obj named s2 of string class with arg of "This sentence will be used to pull 
+	//out substring from it.
+	//CODE:
+
+	//Output call substr() with arguments 14 and 12 on the s2 object
+	//Means start at 14 and then pull 12 characters.
+	//CODE:
+
+}
---
+++ 2.73OpiFinalVideoSeriesFinal/1.73OpiFinalVideoSeriesFinal/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int main() {
+
+
+	//Instantiate string object named s1 of string class with sentence about your 
+	//understanding which is basically nothing.
+	//CODE:
+
+	//Output to show s1.
+	//CODE:
+
+	//Use replace() to take out word in sentence and replace it with "OPPIE" and output to screen.
+	//s1.replace() contains 3 parameters. 1. How many elements to count 2. followed by how many
+	//elements of substring we are replacing 3. And what to replace those elements/characters with.
+	//CODE:
+
+	//Call replace() again with arguments 17, 4,"KANOPI" on the s1 object. Then output new
+	//sentence to the screen (s1 object).
+	//CODE:
+
+	//insert() contains two parameters. 1. Number of elements
+	//to count. 2. What to insert into the string at index location whatever you want.
+
+	//Call the insert() function with arguments with whatever you want.
+	//CODE:
+
+	//Call replace() again with arguments 0, 5, "Adam" on s1 object and add
+	//output statement to show that sentence (s1 object) to the screen.
+	//CODE:
+	
+}
